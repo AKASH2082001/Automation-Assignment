9c39e047d2cbfd286795819abc3515bfb4b605d0
AKASH2082001 <akashbme208@gmail.com>
2022-03-25 02:43:58 -0700

    Automation Assignment

---
+++ Mathsoperations.py
@@ -0,0 +1,48 @@
+import unittest
+
+def Addition(x,y):
+    return x+y
+def Addition1(x,y,z):
+    return x+y+z
+def Subratction(x,y):
+    return x-y
+def Multiplication(x,y):
+    return x*y
+def Division(x,y):
+    return x/y
+
+class Mathsoperationsapp(unittest.TestCase):
+
+    def test_case_Additionof2numbers(self):
+        a= 10
+        b= 10
+        c = a+b
+        self.assertEqual(a+b,c)
+
+    def test_case_Additionof3number(self):
+        a=10
+        b=5
+        c=5
+        d= a+b+c
+        self.assertEqual(a+b+c,d)
+
+    def test_case_suboftwonumber(self):
+        a=10
+        b=5
+        c=a-b
+        self.assertEqual(a-b,c)
+
+    def test_case_muloftwonumber(self):
+        a=5
+        b=2
+        c=a*b
+        self.assertEqual(a*b,c)
+
+    def test_case_divoftwonumber(self):
+        a=10
+        b=5
+        c=a/b
+        self.assertEqual(a/b,c)
+
+if __name__ == "__main__":
+    unittest.main()
